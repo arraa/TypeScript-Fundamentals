6fd066b29312d77dbf12ae69c644be7b96c50636
arraa <tiararahmah82@gmail.com>
2024-03-03 15:51:33 +0700

    Lesson 11

---
+++ Lesson11/src/main.ts
@@ -0,0 +1,47 @@
+import "./css/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItems";
+import ListTemplate from "./Template/ListTemplate";
+
+const initApp = (): void => {
+    const fullList = FullList.instance;
+    const template = ListTemplate.instance;
+
+    const itemEntryForm = document.getElementById(
+        "itemEntryForm"
+    ) as HTMLFormElement;
+
+    itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+        event.preventDefault(); //supaya ga di reload
+
+        const input = document.getElementById("newItem") as HTMLInputElement;
+        const newEntryText: string = input.value.trim();
+        if (!newEntryText.length) return;
+
+
+        const itemId: number = fullList.list.length
+            ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+            : 1;
+
+         const newItem = new ListItem(itemId.toString(), newEntryText);
+       
+        fullList.addItem(newItem);
+        
+        template.render(fullList);
+    });
+
+    const clearItems = document.getElementById(
+        "clearItemsButton"
+    ) as HTMLButtonElement;
+
+    clearItems.addEventListener("click", (): void => {
+        fullList.clearList();
+        template.clear();
+    });
+
+    fullList.load();
+    
+    template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
